b8047e2b1fa0d2e95417740f078aeb8a58f077e5
SuperValou <valentin.esmieu@tuta.io>
2020-05-03 00:00:00 +0200

    Experimenting stuff but it doesn't work really well

--- Integration/Assets/Scripts/Visors/VisorSystem.cs
+++ Integration/Assets/Scripts/Visors/VisorSystem.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering.LWRP;
+using UnityEngine.Rendering;
 using UnityEngine.Rendering.LWRP;
 
 namespace Assets.Scripts.Visors
 {
     public class VisorSystem : MonoBehaviour
     {
-        public Shader radioShader;
-        public Camera eye;
-
-        public ForwardRendererData renderData;
-
-        // Update is called once per frame
+        void Start()
+        {
+        
+        }
+        
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.R))
@@ -28,35 +28,14 @@ namespace Assets.Scripts.Visors
 
         private void ActivateRadioVisor()
         {
-            foreach (RenderObjects scriptableRendererFeature in renderData.rendererFeatures)
-            {
-                
-            }
-
             print("Radio");
-            eye.SetReplacementShader(radioShader, "");
-            eye.cullingMask |= LayerMap.GetXRayLayer();
+            
         }
 
         private void Reset()
         {
             print("Normal");
-            eye.ResetReplacementShader();
-
-            eye.cullingMask &= ~LayerMap.GetXRayLayer();
-        }
-    }
-
-    public static class LayerMap
-    {
-        private const int XRayLayer = 10;
-
-        public static LayerMask GetXRayLayer()
-        {
-            return new LayerMask()
-            {
-                value = 1 << 10
-            };
+            
         }
     }
 }
