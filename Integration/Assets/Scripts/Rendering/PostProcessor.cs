586460b0ca140cadda2e1a939d1b8b3261b55e7c
SuperValou <valentin.esmieu@tuta.io>
2020-05-05 00:44:16 +0200

    Add on-and-off switch

--- Integration/Assets/Scripts/Rendering/PostProcessor.cs
+++ Integration/Assets/Scripts/Rendering/PostProcessor.cs
@@ -2,14 +2,34 @@
 
 namespace Assets.Scripts.Rendering
 {
-    [ExecuteInEditMode]
+//    [ExecuteInEditMode]
     public class PostProcessor : MonoBehaviour
     {
         public Material postProcessMaterial;
 
         void Start()
         {
-            Camera.main.SetReplacementShader(postProcessMaterial.shader, "");
+            
+        }
+
+        private bool xrayEnabled = false;
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (xrayEnabled)
+                {
+                    Camera.main.ResetReplacementShader();
+                    Camera.main.clearFlags = CameraClearFlags.Skybox;
+                }
+                else
+                {
+                    Camera.main.SetReplacementShader(postProcessMaterial.shader, "");
+                    Camera.main.clearFlags = CameraClearFlags.SolidColor;
+                }
+
+                xrayEnabled = !xrayEnabled;
+            }
         }
 
         //void OnRenderImage(RenderTexture src, RenderTexture dest)
